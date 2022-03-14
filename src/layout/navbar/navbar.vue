42b6cc80c7ec0a90f71ecff38655344c05abbcb4
xiaoxunyao <huchengyea@163.com>
2022-03-14 10:22:29 +0800

    chore: init

---
+++ src/layout/navbar/navbar.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<style scoped>
+  @import url('https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css');
+
+</style>
