788a7126831674b825c18751e8c37f3d389c5061
xiaoxunyao <huchengyea@163.com>
2022-03-28 14:58:56 +0800

    fix: update

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import routers from './router/index'
 import App from './App.vue'
-import './styles/main.css'
+import './styles/main.scss'
 
 const app = createApp(App)
 app.use(routers)
